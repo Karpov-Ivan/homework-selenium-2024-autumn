0022eaac07def26d5d3cfd568a8c9f13587d8779
Иван Карпов <ivankarpov@MacBook-Pro-Ivan.local>
2024-12-27 13:55:10 +0300

    Fixed locators

--- hw/code/ui/locators/budget_page_locators.py
+++ hw/code/ui/locators/budget_page_locators.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 class BudgetPageLocators:
     TAB_BUDGET = (By.XPATH, "//span[contains(text(), 'Бюджет')]")
     BUTTON_RECHARGE = (By.CLASS_NAME, "vkuiButton__in")
-    BUTTON_RECHARGE_POPUP = (By.CSS_SELECTOR, ".CreateInvoiceModal_button__DhY5V.vkuiButton.vkuiButton--size-l.vkuiButton--mode-primary.vkuiButton--appearance-accent.vkuiButton--align-center.vkuiButton--sizeY-none.vkuiTappable.vkuiInternalTappable.vkuiTappable--hasHover.vkuiTappable--hasActive.vkui-focus-visible")
+    BUTTON_RECHARGE_POPUP = (By.XPATH, "//button[@type='submit' and .//span[text()='Пополнить счёт']]")
 
     INPUT_PAYMENT_AMOUNT = (By.NAME, "amount")
     INPUT_PAYMENT_AMOUNT_WITHOUT_VAT = (By.NAME, "amountWithoutVat")
@@ -14,5 +14,5 @@ class BudgetPageLocators:
     TAB_BONUS_PROGRAM = (By.XPATH, "//span[contains(text(), 'Бонусная программа')]")
     ACTIVATE_PROMOCODE_BUTTON = (By.XPATH, "//button[contains(@class, 'CouponBanner_button') and .//span[text()='Активировать промокод']]")
 
-    INPUT_PROMOCODE = (By.CSS_SELECTOR, ".vkuiTypography.vkuiInput__el.vkuiText.vkuiText--sizeY-none")
+    INPUT_PROMOCODE = (By.XPATH, "//input[@placeholder='Введите промокод']")
     BUTTON_PROMOCODE = (By.XPATH, "//button[@type='submit' and contains(@class, 'vkuiButton') and span/span[text()='Активировать']]")
