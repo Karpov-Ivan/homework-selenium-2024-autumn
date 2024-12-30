1618995adc3aba9fa6c50352ca43f54579eb1576
Fedasov <fedasov03@inbox.ru>
2024-12-30 17:31:58 +0300

    fix login

--- hw/code/ui/locators/login_page_locators.py
+++ hw/code/ui/locators/login_page_locators.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 
 
 class LoginPageLocators:
-    CABINET_BUTTON = (By.CLASS_NAME, "ButtonCabinet_primary__LCfol")
+    CABINET_BUTTON = (By.CLASS_NAME, "ButtonCabinet_primary")
     MAIL_BUTTON = (By.CSS_SELECTOR, "[data-test-id='oAuthService_mail_ru']")
     USERNAME_INPUT = (By.NAME, "username")
     NEXT_BUTTON = (By.CSS_SELECTOR, "[data-test-id='next-button']")
